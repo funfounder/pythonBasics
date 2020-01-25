08256e9deaf2aa81b94cfbf3ffeb440fd9d2bcaa
Sergei Kalinichenko <kalinichenko.minsk@yandex.ru>
2020-01-25 11:22:56 +0300

    solved with help of other users solutions

--- less02_task02.py
+++ less02_task02.py
@@ -1,5 +1,14 @@
-user_list = [1, 2, 3, 'string', None, True, 4.0]
-new_list = []
+user_list = input('Insert numbers splitted with spaces: ').split()
+new_list = list(user_list) #создает копию, функцию использовать нельзя, а если поставить равно без какой-либо операции, то перезаписывает исходный лист
+for i in range(1, len(new_list), 2):
+    new_list[i-1], new_list[i] = new_list[i], new_list[i-1]
 
-print(user_list)
-print(new_list)
+print(f'Your list was {user_list}')
+print(f'New list was {new_list}')
+
+# i = 0
+# while i+1 <len(user_list):
+#     if i % 2 == 0:
+#         user_list.insert(i, a.pop(i + 1))
+#     i +=1
+# для себя. в if - работаем с одним аргументом, просто получается, что pop откусывает четный и сдвигает его влево. он был i+1, а стал i. а i-тый элемент автоматически сдвигается влево.
--- less02_task03.py
+++ less02_task03.py
@@ -1,3 +1,5 @@
+#подсмотрел и добавил реализация ошибки некорректного месяца
+
 user_month = int(input('Insert month count: '))
 month_dict = {
     'spring': [3, 4, 5],
@@ -6,6 +8,9 @@ month_dict = {
     'winter': [12, 1, 2]
 }
 
+while user_month < 0 or user_month > 12:
+    user_month = int(input('You have entered wrong month. Insert month count: '))
+
 for key, value in month_dict.items():
     if value[0] == user_month:
         print(f'You have inserted {key}')
@@ -13,6 +18,7 @@ for key, value in month_dict.items():
         print(f'You have inserted {key}')
     elif value[2] == user_month:
         print(f'You have inserted {key}')
+
 
 month_list = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter']
 print(f'The month refferes to {month_list[user_month-1]}')
