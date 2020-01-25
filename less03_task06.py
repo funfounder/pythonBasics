eb61ac79ca0ee510b2fe7beddb4b1c50a07bffe3
Sergei Kalinichenko <kalinichenko.minsk@yandex.ru>
2020-01-25 16:31:44 +0300

    changes to lambda in task 6

--- less03_task06.py
+++ less03_task06.py
@@ -14,7 +14,7 @@ my_func()
 #lamba чтобы разобраться как это работает.
 #но тогда пропадает формат. можно как-то сохранить формат при использовании ламба?
 #
-# my_func = lambda words: print(words.title())
+# my_func = lambda words: print(words[0].upper() + words[1:]))
 #
 # my_func(input('Insert the line: '))
 
