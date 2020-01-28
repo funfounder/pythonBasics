82633ee77baad1a88cf9dff94e54933fa68fbbba
Sergei Kalinichenko <kalinichenko.minsk@yandex.ru>
2020-01-28 19:05:10 +0300

    lesson 7 solution

--- less04_task07.py
+++ less04_task07.py
@@ -1,12 +1,14 @@
 from math import factorial
 
-def fact():
-    global n
-    n = int(input('Input number: '))
-    yield factorial(n)
-
-f = fact()
-print(f)
+# def fact():
+#     global n
+#     n = int(input('Input number: '))
+#     yield factorial(n)
+#
+# f = fact()
+#
+# for i in f:
+#     print(i)
 
 # my_list = [i for i in range(fact())]
 # print(my_list)
@@ -18,3 +20,14 @@ print(f)
 #         c +=1
 #     else:
 #         break
+
+def fibo_gen(n):
+    m = 1
+    for i in range(1, n):
+        if i > 15:
+            break
+        m *= i
+        yield m
+
+for i in fibo_gen(26):
+    print(i)
